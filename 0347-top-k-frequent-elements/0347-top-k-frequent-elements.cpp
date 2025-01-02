a1493a6f18ea33fd40ac44211dc9930a52d6cac9
saibendi <saibendi@umich.edu>
2025-01-02 11:02:27 -0600

    Time: 6 ms (28.26%), Space: 19.7 MB (9.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m; 
-        for (const int num : nums) {
+        unordered_map<int,int> m; 
+        for (auto num : nums) {
             m[num]++;
         }
 
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; 
+        vector<vector<int>> buckets(nums.size() + 1);
         for (auto pair : m) {
-            // the order switch is probably what provides key to heap  
-            pq.push({pair.second, pair.first});
-            if (pq.size() > k) {
-                pq.pop();
-            }
+            buckets[pair.second].push_back(pair.first);
         }
 
         vector<int> res; 
-        for (int i = 0; i < k; ++i) {
-            res.push_back(pq.top().second);
-            pq.pop();  
+        for (int i = nums.size(); i > 0; --i) {
+            for (auto val : buckets[i]) {
+                res.push_back(val); 
+                if (res.size() == k) {
+                    return res;
+                }
+            }
         }
-        return res; 
+        return res;
     }
 };
