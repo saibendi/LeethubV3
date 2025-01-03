f11758624f89628deaa8b29b2608813a7d7e3daf
saibendi <saibendi@umich.edu>
2025-01-02 22:16:49 -0600

    Time: 26 ms (26.69%), Space: 25.9 MB (45.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> map; 
-        for (auto s : strs) {
-            vector<int> count(26,0);
-            for (auto c : s) {
-                count[c - 'a']++;
-            }
-            string key = to_string(count[0]);
-            for (int i = 1; i < 26; ++i) {
-                key = key + "," + to_string(count[i]);
-            }
-            map[key].push_back(s);
+        unordered_map<string, vector<string>> mp;
+        
+        for(auto x: strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
         }
-        vector<vector<string>> res; 
-        for (auto pair : map) {
-            res.push_back(pair.second);
+        
+        vector<vector<string>> ans;
+        for(auto x: mp){
+            ans.push_back(x.second);
         }
-        return res; 
+        return ans;
     }
 };
