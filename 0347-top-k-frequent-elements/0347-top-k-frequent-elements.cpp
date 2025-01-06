a0b7f2c0f11a15ad569bbd228af7d6c079d67180
saibendi <saibendi@umich.edu>
2025-01-05 20:52:23 -0600

    Time: 4 ms (43.14%), Space: 20 MB (7.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> m; 
+        unordered_map<int, int> map; 
+        // bst map uses first element to order 
         for (auto num : nums) {
-            m[num]++;
+            map[num]++;
         }
 
-        vector<vector<int>> buckets(nums.size() + 1);
-        for (auto pair : m) {
+        vector<vector<int>> buckets(nums.size()+1, vector<int>(0));
+        for (auto pair : map) {
             buckets[pair.second].push_back(pair.first);
         }
-
+        
         vector<int> res; 
-        for (int i = nums.size(); i > 0; --i) {
+        for (int i = buckets.size() - 1; i > 0; --i) {
             for (auto val : buckets[i]) {
                 res.push_back(val); 
                 if (res.size() == k) {
-                    return res;
+                    return res; 
                 }
             }
         }
-        return res;
+        return res; 
     }
 };
