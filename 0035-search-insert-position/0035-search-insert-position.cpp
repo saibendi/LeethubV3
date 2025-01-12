bcb3432f4ef93730af9630699907d02764d799d4
saibendi <saibendi@umich.edu>
2025-01-11 23:26:51 -0500

    Time: 0 ms (100%), Space: 13.7 MB (30.93%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0; 
+        int r = nums.size(); 
+        while (l < r) {
+            int mid = l + (r-l) /2;
+            if (nums[mid] >= target) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return l; 
+    }
+};
