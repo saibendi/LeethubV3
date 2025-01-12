97ddce5f4c9e8c5fa0d4c8050965411cc8514f87
saibendi <saibendi@umich.edu>
2025-01-11 23:18:10 -0500

    Time: 0 ms (100%), Space: 31.3 MB (36.08%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0; int r = nums.size() - 1; 
-        while (l <= r) {
-            int mid = l + (r-l) / 2;
-            if (target > nums[mid]) {
-                l = mid + 1;
-            }
-            else if (target < nums[mid]) {
-                r = mid - 1;
+        int i = 0; int j = nums.size();
+        while (i < j) {
+            int mid = i + (j-i)/ 2;
+            if (target <= nums[mid]) {
+                j = mid; 
             }
             else {
-                return mid;
+                i = mid + 1;
             }
         }
-        return -1;
+        return i < nums.size() && nums[i] == target ? i : -1;
     }
 };
